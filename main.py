6c42690b668a96db13a4d0c8679f663b961cec5e
minjaeli <minjaelee@comcom.ai>
2025-10-29 07:11:20 +0900

    update: prompt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import dotenv
 
-from tools import count_letters
-
 dotenv.load_dotenv()
 
 from crewai import Crew, Agent, Task
@@ -9,67 +7,51 @@ from crewai.project import CrewBase, agent, task, crew
 
 
 @CrewBase
-class CreativeWriterCrew:
+class NewsReaderAgent:
 
     @agent
-    def creative_writer_agent(self):
+    def news_hunter_agent(self):
         return Agent(
-            config=self.agents_config["creative_writer_agent"],
-        )
-
-    @task
-    def count_task_1(self):
-        return Task(
-            config=self.tasks_config["count_task"],
-        )
-
-    @task
-    def summarize_task(self):
-        return Task(
-            config=self.tasks_config["summarize_task"],
-        )
-
-    @task
-    def count_task_2(self):
-        return Task(
-            config=self.tasks_config["count_task"],
-        )
-
-    @task
-    def rewrite_task(self):
-        return Task(
-            config=self.tasks_config["rewrite_task"],
-        )
-
-    @task
-    def count_task_3(self):
-        return Task(
-            config=self.tasks_config["count_task"],
+            config=self.agents_config["news_hunter_agent"],
         )
 
     @agent
-    def counter_agent(self):
+    def summarizer_agent(self):
         return Agent(
-            config=self.agents_config["counter_agent"],
-            tools=[count_letters],
+            config=self.agents_config["summarizer_agent"],
+        )
+
+    @agent
+    def curator_agent(self):
+        return Agent(
+            config=self.agents_config["curator_agent"],
         )
 
     @task
-    def count_task(self):
+    def content_harvesting_task(self):
         return Task(
-            config=self.tasks_config["count_task"],
+            config=self.tasks_config["content_harvesting_task"],
+        )
+
+    @task
+    def summarization_task(self):
+        return Task(
+            config=self.tasks_config["summarization_task"],
+        )
+
+    @task
+    def final_report_assembly_task(self):
+        return Task(
+            config=self.tasks_config["final_report_assembly_task"],
         )
 
     @crew
-    def assemble_crew(self):
+    def crew(self):
         return Crew(
-            agents=self.agents,
             tasks=self.tasks,
+            agents=self.agents,
             verbose=True,
         )
 
-CreativeWriterCrew().assemble_crew().kickoff(
-    inputs={
-        "text": "The city lights shimmered across the river as the crowd gathered to celebrate the new year.",
-    }
-)
+
+NewsReaderAgent().crew().kickoff()
